e71f6d02d716151b061b0c28cd125e4314fcd03b
Yuan Loong <yuanloong.sum@stengg.com>
2024-09-23 14:53:08 +0800

    v1 for application controller

--- express/controllers/app.js
+++ express/controllers/app.js
@@ -1,5 +1,5 @@
 /**
- * App Controller
+ * Application Controller
  */
 
 const pool = require("../utils/db");
@@ -12,22 +12,96 @@ const groupC = require("./group");
  * @param {*} res
  * @param {*} next
  */
-exports.getAllApp = async (req, res, next) => {};
+exports.getAllApp = async (req, res, next) => {
+	try {
+		let [val] = await pool.query(
+			"select app_acronym, app_description, app_Rnumber from application"
+		);
+		res.status(200).json({
+			success: true,
+			applist: val
+		});
+	} catch (error) {
+		console.log("error getting the apps");
+	}
+};
 
 /**
  * Get 1 app
  */
-exports.getApp = async (req, res, next) => {};
+exports.getApp = async (req, res, next) => {
+	let { app_acronym } = req.body;
+
+	//app_acronym validation
+
+	try {
+		let [val] = await pool.execute(
+			"select app_acronym, app_description, app_Rnumber from application where app_acronym = ?",
+			[app_acronym]
+		);
+		if (val.length == 0) {
+			console.log("no such app found");
+		}
+		res.status(200).json({
+			success: true,
+			applist: val
+		});
+	} catch (error) {
+		console.log("error getting the apps");
+	}
+};
 
 /**
  * Updates 1 app
  * Pre-req: User has group-level access to manage app
+ * TODO: go through and check this is generated
  * @param {string} app_id
  * @param {*} res
  * @param {*} next
  */
-exports.updateApp = async (req, res, next) => {};
+exports.updateApp = async (req, res, next) => {
+	let { app_acronym, R_number, description, startDate, endDate } = req.body;
+	let { permit_create, permit_open, permit_todo, permit_doing, permit_done } =
+		req.body;
 
+	// Check for required fields
+	if (!app_acronym || !R_number || !description || !startDate || !endDate) {
+		return next("Required fields are missing");
+	}
+
+	// Validate R_number
+	if (!Number.isInteger(R_number)) {
+		return next("R_number provided is not a whole number");
+	}
+
+	// Check if app_acronym already exists (excluding the current app)
+	try {
+		let [val] = await pool.execute(
+			`SELECT app_Rnumber FROM application WHERE app_acronym = ?`,
+			[app_acronym]
+		);
+		if (val.length == 0) {
+			return next(
+				new ErrorObj("app_acronym does not exist system", 400, "")
+			);
+		}
+	} catch (err) {
+		return next(new ErrorObj("Database error", 500, ""));
+	}
+
+	// Update the application record
+	try {
+		let [val] = await pool.execute(
+			"UPDATE application SET app_description = ?, app_Rnumber = ?, app_startDate = ?, app_endDate = ? WHERE app_acronym = ?",
+			[app_acronym, description, R_number, startDate, endDate]
+		);
+		res.status(200).json({
+			success: true
+		});
+	} catch (error) {
+		return next("Error with update");
+	}
+};
 // What to do for change state
 
 /**
@@ -36,4 +110,53 @@ exports.updateApp = async (req, res, next) => {};
  * @param {*} res
  * @param {*} next
  */
-exports.createApp = async (req, res, next) => {};
+exports.createApp = async (req, res, next) => {
+	let { app_acronym, R_number, description, startDate, endDate } = req.body;
+	let { permit_create, permit_open, permit_todo, permit_doing, permit_done } =
+		req.body;
+
+	/**
+	 * checks to do
+	 * mandatory app_acronm, R_number, startDate, endDate
+	 * check format and validity of the start and end date
+	 * If app_acronym already exists
+	 */
+
+	if (!app_acronym || !R_number || !description || !startDate || !endDate) {
+		return next("Required fields are missings");
+	}
+
+	try {
+		let [val] = await pool.execute(
+			`Select app_Rnumber from application where app_acronym = ?`,
+			[app_acronym]
+		);
+		if (val.length > 0) {
+			return next(
+				new ErrorObj(
+					"app_acronym already exists in the system",
+					400,
+					""
+				)
+			);
+		}
+	} catch (err) {
+		return next(new ErrorObj("Database error", 500, ""));
+	}
+
+	if (!Number.isInteger(R_number)) {
+		return next("R_number provided is not a whole number");
+	}
+
+	try {
+		let [val] = await pool.execute(
+			"insert into `application` (`app_acronym`, `app_description`, `app_Rnumber`, `app_startDate`, `app_endDate` ) values (?,?,?,?,?)",
+			[app_acronym, description, R_number, startDate, endDate]
+		);
+		res.status.json({
+			success: true
+		});
+	} catch (error) {
+		return next("error with insertion");
+	}
+};
