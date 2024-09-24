d8ac9c50baa5569aa250aed11d4bf1d11f89d467
Yuan Loong <yuanloong.sum@stengg.com>
2024-09-24 17:39:05 +0800

    basic from applist to kanban to backend

--- FE/src/routes/kanban/+page.svelte
+++ FE/src/routes/kanban/+page.svelte
@@ -1,36 +1,21 @@
 <script>
 	import TaskCard from '$lib/TaskCard.svelte';
 	import { page } from '$app/stores';
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+	import { axios } from '$lib/config.js';
 
-	let tasks = [
-		{
-			id: 1,
-			name: 'Task 1',
-			description: 'Description 1',
-			owner: 'Alice',
-			state: 'todo',
-			color: 'red'
-		},
-		{
-			id: 1,
-			name: 'Task 1',
-			description: 'Description 1',
-			owner: 'Alice',
-			state: 'done',
-			color: 'blue'
-		},
-		{
-			id: 1,
-			name: 'Task 1',
-			description: 'Description 1',
-			owner: 'Alice',
-			state: 'doing',
-			color: 'green'
-		}
-		// Add more tasks
-	];
+	$: tasks = [];
 
-	const columns = {
+	$: {
+		tasks.forEach((task) => {
+			if (columns[task.state]) {
+				columns[task.state].push(task);
+			}
+		});
+		columns = columns; //bit of a hack to force the columns to refresh
+	}
+
+	let columns = {
 		open: [],
 		todo: [],
 		doing: [],
@@ -38,12 +23,49 @@
 		close: []
 	};
 
-	tasks.forEach((task) => {
-		if (columns[task.state]) {
-			columns[task.state].push(task);
-		}
+	async function onLoad(app) {
+		try {
+			let res1 = await axios.post('/tms/tasks/all', {
+				app_name: app
+			});
+			console.log(res1);
+			if (res1.data.success) {
+				console.log(res1.data.taskList);
+			}
+		} catch (error) {}
+	}
+
+	onMount(async () => {
+		console.log($page.state.app);
+		await onLoad($page.state.app);
+		tasks = [
+			{
+				id: 1,
+				name: 'Task 1',
+				description: 'Description 1',
+				owner: 'Alice',
+				state: 'todo',
+				color: 'red'
+			},
+			{
+				id: 1,
+				name: 'Task 1',
+				description: 'Description 1',
+				owner: 'Alice',
+				state: 'done',
+				color: 'blue'
+			},
+			{
+				id: 1,
+				name: 'Task 1',
+				description: 'Description 1',
+				owner: 'Alice',
+				state: 'doing',
+				color: 'green'
+			}
+			// Add more tasks
+		];
 	});
-	console.log($page.state);
 </script>
 
 <div class="board">
